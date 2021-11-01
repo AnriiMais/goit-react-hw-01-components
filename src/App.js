7cc6f030c393ac1d7daee3208e41ecb81a16f43e
Andrii <maystrenko22a@gmail.com>
2021-11-01 17:45:15 +0200

    hw

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
-import './App.css';
+import Profile from './Profile/Profile';
+import user from './Profile/user.json';
+import Statistics from './Statistics';
+import statisticsData from './Statistics/statistical-data.json';
+import FriendList from './FriendList';
+import friends from './FriendList/friends.json';
+import TransactionHistory from './TransactionHistory';
+import transactions from './TransactionHistory/transactions.json';
 
+import './App.css';
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <Profile
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={statisticsData} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </>
+  );
 }
 
 export default App;
